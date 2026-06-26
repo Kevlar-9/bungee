f4f67125b84b406856cb04247443b7c3e251449e
Francis Siripi <siripifrancis21@gmail.com>
2026-06-26 16:06:47 +0000

    update for today

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,8 +6,8 @@ function navbar() {
 
   return (
 
-    <div className='bg-transparent flex p-4 w-screen items-center justify-between border-b border-[#D9D9D9]'>
-      <div className='flex items-center gap-1 w-full'>
+    <div className='bg-transparent flex p-4 w-screen items-center justify-between border-b-2 border-[#A33800]'>
+      <div className='flex items-center gap-1 flex-shrink-0'>
         <img className="w-15" src={logo} alt="bungee logo" />
         <SplitText
           text="Bungee"
@@ -23,12 +23,12 @@ function navbar() {
           showCallback
         />
       </div>
-      <div className='text-sm text-white flex items-center w-full justify-evenly'>
+      <div className='text-sm text-white flex items-center flex-1 justify-center gap-8 font-hanken tracking-wider'>
         <NavLink to='/categories' className={({ isActive }) => `hover:italic transition duration-150 ${isActive ? 'underline' : ''}`}>Categories</NavLink>
         <NavLink to='/about' className={({ isActive }) => `hover:italic transition duration-150 ${isActive ? 'underline' : ''}`}>About</NavLink>
         <NavLink to='/contact' className={({ isActive }) => `hover:italic transition duration-150 ${isActive ? 'underline' : ''}`}>Contact</NavLink>
       </div>
-      <div className='w-full flex items-center justify-center gap-4'>
+      <div className='flex-shrink-0 flex items-center justify-center gap-4'>
         <input type="search" className='focus:outline-0 text-sm px-7 py-3 tracking-widest text-white border-b-2 italic bg-[#D9D9D9]/5 border-[#0040E0]' placeholder='Seach Insights...' />
         <button className='bg-[#0040E0] px-10 py-3 rounded-md text-sm text-white tracking-wider hover:bg-[#0040E0]/80'>Jump In</button>
       </div>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -6,9 +6,46 @@ function contact() {
   return (
     <div className='bg-sky-600 h-screen'>
       <Navbar />
+        <main>
+              <div className='bg-red-100 flex flex-col justify-between gap-3 p-5'>
+                <section>
+                  <button className='text-sm bg-[#FFDBCE] rounded-4xl py-2 px-5 font-semibold text-[#370E00]'>GET IN TOUCH</button>
+                </section>
+                <section>
+                  <h1 className='text-[#0040E0] text-[62px] font-anybody font-bold leading-[80px]'>Let's take the leap together.</h1>
+                </section>
+                <section>
+                  <p className='text-sm leading-6'>Whether you have a burning about our latest dive into data or just <br /> want to say hi, we're here for the rebound. Friendly vibes only.</p>
+                </section>
+              </div>
 
 
+              <div className='flex w-full justify-between p-3'>
+                  <section>
+                    <form className=''>
+                      <div className='flex'>
+                        <div className='flex flex-col'>
+                          <label className='text-sm'>Your Name</label>
+                          <input type="text" className="" placeholder='John Doe'/>
+                        </div>
+                        <div className='flex flex-col'>
+                          <label className='text-sm'>Email Address</label>
+                          <input type="email" className="" placeholder='john@example.com'/>
+                        </div>
+                      </div>
 
+                    <div className='flex flex-col'>
+                      <label className='text-sm'> What's the topic?</label>
+                      <select>
+                        <option value="">General Inquiry</option>
+                      </select>
+                    </div>
+
+                    </form>
+                  </section>
+                  <section>2</section>
+              </div>
+        </main>
       <Footer />
     </div>
   )
